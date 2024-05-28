d692e052422c587a1f72f6d854233af11722234b
wminsoo1 <wminsoo1@naver.com>
2024-05-28 21:35:31 +0900

    main에서 로직 실행 -> 도메인에서 로직 실행 으로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,30 +4,7 @@ public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
 
-        while (true) {
-
-            BaseballGame baseballGame = new BaseballGame();
-            User user = baseballGame.getUser();
-
-            System.out.println("answer = " + baseballGame.getComputer().getNumbers());
-
-            do {
-
-                user.inputThreeNumber();
-
-                baseballGame.calculateStrikesAndBalls();
-
-                baseballGame.printStrikeAndBall();
-
-            } while (!baseballGame.isStrikeThree());
-
-
-            user.inputOneNumber();
-
-            if (!baseballGame.gameRestart()) {
-                break;
-            }
-        }
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.run();
     }
-
 }
