201bb01fdd0dbd595f8ec935085c8e15931078ac
wminsoo1 <wminsoo1@naver.com>
2024-05-30 09:18:27 +0900

    static 변수 제거

--- src/main/java/baseball/utils/Constant.java
+++ src/main/java/baseball/utils/Constant.java
@@ -1,17 +1,8 @@
 package baseball.utils;
 
 public class Constant {
+
     public static final int LENGTH_OF_NUMBERS = 3;
-    public static final int MIN_NUM = 1;
-    public static final int MAX_NUM = 9;
-
-    public static final String INPUT_NUMBERS = "숫자를 입력해주세요 : ";
-    public static final String INPUT_ONE_OR_TWO = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    public static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    public static final String STRIKE = "스트라이크";
-    public static final String BALL = "볼";
-    public static final String NOTHING = "낫싱";
-
 
     public static final String ERROR_MESSAGE_LENGTH_OF_NUMBERS = "3개의 숫자를 입력해야 합니다";
     public static final String ERROR_MESSAGE_LENGTH_IS_ONE = "1개의 숫자를 입력하세요";
